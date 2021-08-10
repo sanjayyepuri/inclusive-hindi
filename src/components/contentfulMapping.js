8579f3322b396f28decc1c709c6dd3768e676899
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2021-08-10 00:39:23 -0500

    fixed missing team member mapping

--- src/components/contentfulMapping.js
+++ src/components/contentfulMapping.js
@@ -2,12 +2,14 @@ import Block from "./block";
 import Splash from "./splash";
 import Transcript from "./transcript";
 import TopicList from "./materialList";
+import TeamMember from "./teamMember";
 
 const contentfulMapping = {
   ContentfulPageBlock: Block,
   ContentfulPageSplash: Splash,
   ContentfulMaterialTranscript: Transcript,
   ContentfulTopic: TopicList,
+  ContentfulTeamMember: TeamMember
 };
 
 export default contentfulMapping;
